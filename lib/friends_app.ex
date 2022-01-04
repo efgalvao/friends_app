c7215a79ed8fc7443d37f7c545fe415467cadf34
efgalvao <solfieri@hotmail.com>
2022-01-04 15:27:35 -0300

    initial commit

---
+++ lib/friends_app.ex
@@ -0,0 +1,19 @@
+defmodule FriendsApp do
+  @moduledoc """
+  Documentation for `FriendsApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FriendsApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+end
